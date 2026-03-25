f8858bb338c49647bcd819a7b3e536c3d4aa8f67
ridarajani <ridarajani125@gmail.com>
2026-03-25 16:30:14 +0500

    Fix gitignore and remove .next from tracking

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Ge/nOUyoDNUsoWyJ4KNatEzy90/cnmmgInzn5xVbEus=\"\n}"
--- .next/dev/static/chunks/src_app_layout_tsx_1cf6b850._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_6c71a16c._.css"
-],
-    source: "dynamic"
-});
